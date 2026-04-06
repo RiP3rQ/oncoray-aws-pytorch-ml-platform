8021135c6cca3dcadc66515544c1a1842620f2c5
Rafał Pompa <108752227+rip3rq@users.noreply.github.com>
2026-04-06 14:49:48 +0200

    Add .gitignore, implement FastAPI endpoints, and update package dependencies

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,6 +1,54 @@
-def main():
-    print("Hello from api!")
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+# =============================== ROOT ENDPOINT ===============================
+
+@app.get("/")
+async def get_root():
+    """
+    Root endpoint for the API.
+    """
+    return {"service": "core-api", "status": "ok"}
+
+# =============================== SCALAR DOCS ENDPOINT ===============================
+
+@app.get("/scalar", include_in_schema=False)
+async def get_scalar_docs():
+    """
+    Adds an Scalar API reference to the API, so we can use it in the Scalar console.
+    """
+    return get_scalar_api_reference(
+        openapi_url="/openapi.json",
+        title="Core API",
+    )
+
+# =============================== KUBERNETES READINESS ENDPOINT ===============================
+
+@app.get("/livez")
+async def get_livez():
+    """
+    Kubernetes readiness endpoint.
+    """
+    return {"status": "ok"}
+
+# =============================== KUBERNETES LIVENESS ENDPOINT ===============================
+
+@app.get("/readyz")
+async def get_readyz():
+    """
+    Kubernetes liveness endpoint.
+    """
+    return {"status": "ok"}
+
+# =============================== KUBERNETES HEALTH ENDPOINT ===============================
+
+@app.get("/health")
+async def get_health():
+    """
+    Kubernetes health endpoint.
+    """
+    return {"status": "ok"}
+
+# =============================== MAIN ===============================
