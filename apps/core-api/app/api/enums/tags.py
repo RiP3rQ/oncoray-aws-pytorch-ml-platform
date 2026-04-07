cc22b838fb4ca73e490cc70dcb6b39d7e0ebd064
Rafał Pompa <108752227+rip3rq@users.noreply.github.com>
2026-04-07 20:10:41 +0200

    Remove core-api application and all related files.

--- apps/core-api/app/api/enums/tags.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class APITag(str, Enum):
-    """API Tags for the Core API"""
-    USER = "User"
-    MODEL = "Model"
--- apps/core-api/app/api/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.routers import user
-
-
-master_router = APIRouter()
-master_router.include_router(user.router)
