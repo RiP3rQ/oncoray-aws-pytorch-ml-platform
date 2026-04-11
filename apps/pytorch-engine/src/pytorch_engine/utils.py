fb63b66e8be089cbacc5efa6e1414326ea76764d
Rafał Pompa <108752227+rip3rq@users.noreply.github.com>
2026-04-11 17:13:38 +0200

    feat: add `set_seeds` utility to configure random seeds for reproducibility
    - Introduced `set_seeds` function in `utils.py` to allow setting random seeds for PyTorch and CUDA operations.
    - Included default seed value of 42 with customization support through function argument.

--- apps/pytorch-engine/src/pytorch_engine/utils.py
+++ apps/pytorch-engine/src/pytorch_engine/utils.py
@@ -34,3 +34,15 @@ def resolve_device(device: str | torch.device) -> torch.device:
 def get_current_device() -> torch.device:
     """Get the current device."""
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def set_seeds(seed: int = 42):
+    """Sets random sets for torch operations.
+
+    Args:
+        seed (int, optional): Random seed to set. Defaults to 42.
+    """
+    # Set the seed for general torch operations
+    torch.manual_seed(seed)
+    # Set the seed for CUDA torch operations (ones that happen on the GPU)
+    torch.cuda.manual_seed(seed)
