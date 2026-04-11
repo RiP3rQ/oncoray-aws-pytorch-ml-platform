ac91c98bbc23250fef34fb0d2bd9722e63a557d4
Rafał Pompa <108752227+rip3rq@users.noreply.github.com>
2026-04-11 16:03:25 +0200

    chore: comment out incomplete TinyVGG integration and update TODOs
    - Commented out references to unimplemented `model_builder.TinyVGG` to prevent runtime issues.
    - Added TODOs for future implementation of `model_builder` module and TinyVGG class.
    - Temporarily disabled related training and saving logic for modular design readiness.

--- apps/pytorch-engine/src/pytorch_engine/train_model.py
+++ apps/pytorch-engine/src/pytorch_engine/train_model.py
@@ -2,13 +2,11 @@
 Trains a PyTorch image classification model using device-agnostic code.
 """
 
-import engine
-import model_builder
 import torch
-import utils
 from torchvision import transforms
 
-import data_setup
+# TODO: model_builder module with TinyVGG class not yet implemented
+# from . import model_builder
 
 # Setup hyperparameters
 NUM_EPOCHS = 5
@@ -28,37 +26,37 @@ data_transform = transforms.Compose(
     [transforms.Resize((64, 64)), transforms.ToTensor()]
 )
 
-# Create DataLoaders with help from data_setup.py
-train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
-    train_dir=train_dir,
-    test_dir=test_dir,
-    transform=data_transform,
-    batch_size=BATCH_SIZE,
-)
-
-# Create model with help from model_builder.py
-model = model_builder.TinyVGG(
-    input_shape=3, hidden_units=HIDDEN_UNITS, output_shape=len(class_names)
-).to(device)
-
-# Set loss and optimizer
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
-# Start training with help from engine.py
-engine.train(
-    model=model,
-    train_dataloader=train_dataloader,
-    test_dataloader=test_dataloader,
-    loss_fn=loss_fn,
-    optimizer=optimizer,
-    epochs=NUM_EPOCHS,
-    device=device,
-)
-
-# Save the model with help from utils.py
-utils.save_model(
-    model=model,
-    target_dir="models",
-    model_name="05_going_modular_script_mode_tinyvgg_model.pth",
-)
+# # Create DataLoaders with help from data_setup.py
+# train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+#     train_dir=train_dir,
+#     test_dir=test_dir,
+#     transform=data_transform,
+#     batch_size=BATCH_SIZE,
+# )
+#
+# # TODO: Uncomment when model_builder module with TinyVGG class is implemented
+# # model = model_builder.TinyVGG(
+# #     input_shape=3, hidden_units=HIDDEN_UNITS, output_shape=len(class_names)
+# # ).to(device)
+#
+# # Set loss and optimizer
+# loss_fn = torch.nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+#
+# # Start training with help from training_loop.py
+# training_loop.train_model(
+#     model=model,
+#     train_dataloader=train_dataloader,
+#     test_dataloader=test_dataloader,
+#     loss_fn=loss_fn,
+#     optimizer=optimizer,
+#     epochs=NUM_EPOCHS,
+#     device=device,
+# )
+#
+# # Save the model with help from save_model.py
+# save_model.save_model(
+#     model=model,
+#     model_name="05_going_modular_script_mode_tinyvgg_model.pth",
+#     target_dir="models",
+# )
